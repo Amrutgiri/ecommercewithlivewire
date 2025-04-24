b9e92ba5596a1799bcc4dac749382fba12bf0b34
amrutgiri goswami <amrutgiri.goswami@excellentwebworld.in>
2025-04-24 19:57:23 +0530

    Make Dynamic Filters

--- app/Livewire/ProductsPage.php
+++ app/Livewire/ProductsPage.php
@@ -6,6 +6,7 @@ use App\Models\Brand;
 use App\Models\Category;
 use App\Models\Product;
 use Livewire\Attributes\Title;
+use Livewire\Attributes\Url;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -13,11 +14,45 @@ use Livewire\WithPagination;
 class ProductsPage extends Component
 {
     use WithPagination;
+
+    #[Url]
+    public $selected_categories =[];
+
+    #[Url]
+    public $selected_brands =[];
+
+    #[Url]
+    public $featured;
+
+    #[Url]
+    public $on_sale;
+
+    #[Url]
+    public $price_range = 300000;
+
+
     public function render()
     {
-        $products = Product::where('is_active', 1);
+        $products = Product::query()->where('is_active', 1);
+
+        if(!empty($this->selected_categories)){
+            $products->whereIn('category_id',$this->selected_categories);
+        }
+        if(!empty($this->selected_brands)){
+            $products->whereIn('brand_id',$this->selected_brands);
+        }
+        if(!empty($this->featured)){
+            $products->where('is_featured', 1);
+        }
+        if(!empty($this->on_sale)){
+            $products->where('on_sale', 1);
+        }
+        if(!empty($this->price_range)){
+            $products->whereBetween('price',[0,$this->price_range]);
+        }
+
         return view('livewire.products-page',[
-            'products' => $products->paginate(6),
+            'products' => $products->paginate(9),
             'brands'=> Brand::where('is_active', 1)->get(['id','name','slug']),
             'categories'=> Category::where('is_active', 1)->get(['id','name','slug']),
         ]);
