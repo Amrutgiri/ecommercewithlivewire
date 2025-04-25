ce63e3b3bd507199149ae461a1b6845dc266a042
amrutgiri goswami <amrutgiri.goswami@excellentwebworld.in>
2025-04-25 12:30:00 +0530

    Complete dynamic Quantity and Add to cart in product detail page

--- app/Livewire/ProductDetailPage.php
+++ app/Livewire/ProductDetailPage.php
@@ -2,7 +2,10 @@
 
 namespace App\Livewire;
 
+use App\Helpers\CartManagement;
+use App\Livewire\Partials\Navbar;
 use App\Models\Product;
+use Jantinnerezo\LivewireAlert\Facades\LivewireAlert;
 use Livewire\Attributes\Title;
 use Livewire\Component;
 
@@ -10,7 +13,29 @@ use Livewire\Component;
 class ProductDetailPage extends Component
 {
     public $slug;
+    public $quantity = 1;
 
+    public function decreaseQuantity(){
+        if($this->quantity > 1){
+            $this->quantity--;
+        }
+    }
+    public function increaseQuantity(){
+        $this->quantity++;
+    }
+
+    public function addToCart($product_id){
+        $total_count = CartManagement::addItemToCart($product_id);
+
+        $this->dispatch('update-cart-count',total_count:$total_count)->to(Navbar::class);
+
+       LivewireAlert::title('Product Added To Cart successfully!')
+       ->success()
+       ->toast()
+       ->position('top-end')
+       ->timer(3000)
+       ->show();
+    }
     public function mount($slug){
         $this->slug = $slug;
     }
